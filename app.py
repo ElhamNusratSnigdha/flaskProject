b32a4e8253674c0de0617a38213dc50d80c4e15d
Elham Nusrat <elham.snigdha.ch@gmail.com>
2023-07-29 18:33:42 +0600

    initial

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+#to set-up the application
+app = Flask(__name__) #reference of this file
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+db = SQLAlchemy(app) #the database
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/')
+def index():
+    #return "Hello, World!"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
